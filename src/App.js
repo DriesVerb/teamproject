8144208b9b7ecc7f3c639f7c3b1595b3b7a2a9d7
Syed Moazzam Ali Shah <syed@syeds-mbp.speedport.ip>
2021-03-23 15:10:07 +0100

    small changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 // react-router-dom
-import { Route } from "react-router-dom";
+import { Route } from 'react-router-dom';
 
 // redux
-import { Provider } from "react-redux";
-import store from "./store/Store";
+import { Provider } from 'react-redux';
+import store from './store/Store';
 
 // components home
-import Home from "./components/Home";
-import NavBar from "./components/NavBar";
-import SearchCountry from "./components/Charts";
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import SearchCountry from './components/Charts';
 import TopTracks from './components/TopTracks';
-import MainApp from "./carousel/components/MainApp";
-import AlbumName from "./components/AlbumName"
-
-
+import MainApp from './carousel/components/MainApp';
+import AlbumName from './components/AlbumName';
 
 // components to routes
 
@@ -25,13 +23,11 @@ function App() {
       <Route path='/' exact component={Home} />
       <Route path='/charts' component={SearchCountry} />
       <Route path='/tracks' component={TopTracks} />
-      <Route path="/album" component={AlbumName}/>
+      <Route path='/album' component={AlbumName} />
 
       <MainApp />
-
     </Provider>
   );
 }
 
 export default App;
- 
--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 import {
   fetchMusicCharts,
   changeCountry,
   changeVisible,
   changeBandName,
-} from "../actions/GetMusicAction";
-import { connect } from "react-redux";
+} from '../actions/GetMusicAction';
+import { connect } from 'react-redux';
 
-import BandName from "./BandName";
+import BandName from './BandName';
 
 const SearchCountry = (props) => {
   useEffect(() => {
@@ -41,7 +41,7 @@ const SearchCountry = (props) => {
           return (
             <div key={index}>
               <div
-                style={{ cursor: "pointer" }}
+                style={{ cursor: 'pointer' }}
                 onClick={() => {
                   props.changeBandName(band.name);
                   props.changeVisible();
