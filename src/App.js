f0913e096841bc91edf9eb6593744ec8c18f178b
Syed Moazzam Ali Shah <syed@syeds-mbp.speedport.ip>
2021-03-24 09:24:25 +0100

    deleted something

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 // react-router-dom
-<<<<<<< HEAD
-import { Route, Switch } from "react-router-dom";
-=======
 import { Route } from 'react-router-dom';
->>>>>>> master
 
 // redux
 import { Provider } from 'react-redux';
 import store from './store/Store';
 
 // components home
-<<<<<<< HEAD
-import Home from "./components/Home";
-import NavBar from "./components/NavBar";
-import SearchCountry from "./components/Charts";
-import NotFound from './components/NotFound';
-=======
 import Home from './components/Home';
 import NavBar from './components/NavBar';
 import SearchCountry from './components/Charts';
->>>>>>> master
 import TopTracks from './components/TopTracks';
 import MainApp from './carousel/components/MainApp';
 import AlbumName from './components/AlbumName';
-
 
 // components to routes
 
@@ -31,20 +19,14 @@ function App() {
   return (
     <Provider store={store}>
       <NavBar />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/charts' component={SearchCountry} />
-        <Route path='/tracks' component={TopTracks} />
-        <Route component={NotFound} />
+      <Route path='/' exact component={Home} />
+      <Route path='/charts' component={SearchCountry} />
+      <Route path='/tracks' component={TopTracks} />
 
-<<<<<<< HEAD
-      </Switch>
-=======
       <Route path='/' exact component={Home} />
       <Route path='/charts' component={SearchCountry} />
       <Route path='/tracks' component={TopTracks} />
       <Route path='/album' component={AlbumName} />
->>>>>>> master
 
       <MainApp />
     </Provider>
